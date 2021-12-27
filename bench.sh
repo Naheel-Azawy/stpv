a98c64857cd0debfc7707331928568f52b11b9d0
Naheel-Azawy <naheelazawy@gmail.com>
2021-12-27 22:35:34 +0300

    exp version

---
+++ bench.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# current time in millies
+now() { date "+%s%3N"; }
+
+run() {
+    bin="$1"
+    start=$(now)
+
+    for _ in $(seq 3); do
+        "$bin" ./LICENSE
+        "$bin" ./Makefile
+        "$bin" ./stpv
+        "$bin" ./screenshot.png
+    done >/dev/null
+
+    end=$(now)
+    diff=$((end - start))
+
+    echo "$bin: $diff ms"
+}
+
+run ./stpv
+run ./stpv-exp
